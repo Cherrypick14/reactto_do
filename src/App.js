f5b570a41607b80504076ff8acb258bfb0db9ea7
Cherrypick14 <cherrypicklet@gmail.com>
2023-04-27 09:58:48 +0300

    test the main app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className=" font-bold items center justify-center text-red-600 text-2xl">Hey there, your brain is amazing</h1>
+    </div>
+  );
+}
+
+export default App;
